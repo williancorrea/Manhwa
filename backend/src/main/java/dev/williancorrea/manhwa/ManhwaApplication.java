19379bd6a7791e28f86901c9f91037554fc15726
Willian Vagner Vicente Corrêa <willian.vag@gmail.com>
2026-01-16 18:45:56 -0300

    Initialize Spring Boot project with core configuration and basic structure

---
+++ backend/src/main/java/dev/williancorrea/manhwa/ManhwaApplication.java
@@ -0,0 +1,13 @@
+package dev.williancorrea.manhwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManhwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManhwaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/williancorrea/manhwa/ManhwaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.williancorrea.manhwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManhwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
